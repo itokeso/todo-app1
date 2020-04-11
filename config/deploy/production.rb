7ba43dc49e10c1e7e497ffe50f1e21bf6432592c
itokeso <61177741+itokeso@users.noreply.github.com>
2020-04-11 15:09:04 +0900

    add-basic-auth

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,8 @@ server '54.92.67.93', user: 'ec2-user', roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-
+set :rails_env, "production"
+set :unicorn_rack_env, "production"
 
 
 # role-based syntax
